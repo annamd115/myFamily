8321cb25df2607609446c2e28f4ab348f6fa82af
annamd115 <anna.dolan115@gmail.com>
2018-09-10 20:07:35 -0500

    printing family members

--- TheFamilyDictionary/Program.cs
+++ TheFamilyDictionary/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TheFamilyDictionary
 {
@@ -6,7 +7,40 @@ namespace TheFamilyDictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Define a Dictionary that contains information about several members of your family. Use the following example as a template.
+            Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+
+            myFamily.Add("sister", new Dictionary<string, string>(){
+                {"name", "Megan"},
+                {"age", "22"}
+            });
+
+            myFamily.Add("brother", new Dictionary<string, string>(){
+                {"name", "Corban"},
+                {"age", "27"}
+            });
+
+            myFamily.Add("mother", new Dictionary<string, string>(){
+                {"name", "Michelle"},
+                {"age", "51"}
+            });
+
+            myFamily.Add("father", new Dictionary<string, string>(){
+                {"name", "Kevin"},
+                {"age", "55"}
+            });
+
+            //Next, iterate over each item in myFamily and produce the following output. Krista is my sister and is 42 years old.
+
+            foreach (var member in myFamily)
+            {
+                var name = member.Value["name"];
+                var relation = member.Key;
+                var age = member.Value["age"];
+
+                Console.WriteLine($"{ name } is my { relation } and is { age } years old.");
+            }
+            Console.ReadLine();
         }
     }
 }
